4f46bc30bf078d702bb927afca9d220d1bc8f742
Thullyoo <thullyofilho@gmail.com>
2024-10-22 11:42:40 -0300

    Initial commit

---
+++ src/main/java/com/thullyoo/spring_cache_redis/SpringCacheRedisApplication.java
@@ -0,0 +1,13 @@
+package com.thullyoo.spring_cache_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCacheRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCacheRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thullyoo/spring_cache_redis/SpringCacheRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thullyoo.spring_cache_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCacheRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
